3465ec4b419913c0d20ea4b92a9ae24d771fe331
Nicolas <vassallo.mn@gmail.com>
2021-11-11 14:44:26 -0300

    servicio clima

--- src/app/_service/clima.service.ts
+++ src/app/_service/clima.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClimaService {
 
-  constructor() { }
+
+  key='b3290ab41061431163617beb68d3b208';
+  url= 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+
+
+  constructor(private http: HttpClient){ }
+
+  obtenerClima(ciudad: string): Observable<any> {
+    return this.http.get(`api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${this.key}`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { ClimaComponent } from './pages/clima/clima.component';
 import { NavbarComponent } from './pages/navbar/navbar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
